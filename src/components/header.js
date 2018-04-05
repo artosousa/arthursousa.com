4d459f9ae6533308c1d307a6e5b7b023b26fce5d
Trevor Blades <tdblades@gmail.com>
2018-04-05 23:54:04 +0100

    Add emotion

--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,26 @@
 import React from 'react';
+import styled from 'react-emotion';
+
+// Using javascript objects
+const Wrapper = styled.div({
+  padding: 16,
+  background: 'grey',
+  color: 'white',
+});
+
+// With tagged template literals (writing plain css)
+const Menu = styled.nav`
+  margin-top: 8px;
+  background-color: red;
+`;
 
 const Header = () => (
-  <div
-    style={{
-      padding: 16,
-      background: 'grey',
-      color: 'white',
-    }}
-  >
+  <Wrapper>
     <h1>Mycros</h1>
-    <nav>
+    <Menu>
       <a href="/home">Home</a>
-    </nav>
-  </div>
+    </Menu>
+  </Wrapper>
 );
 
 export default Header;
